da7c4c7fa6ccd37f5d0aa081a7139a3b067abde6
benjik81 <benjamin.zanini@outlook.com>
2021-12-14 17:14:47 +0100

    scripts map

---
+++ MenuAPoint/Assets/Scripts/scrMapManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class scrMapManager : MonoBehaviour
+{
+
+    [Header("Scene")]
+    public Scene maScene;
+    public string sceneName;
+    //public string selectText;
+    public TextAsset text;
+    //public scrTextManager test;
+    //public scrGlobal globalScript;
+    //public TextAsset filename;
+
+    public void LoadNewScene()
+    {
+       SceneManager.LoadScene(sceneName);
+    }
+
+    public void test()
+    {
+        GameObject.Find("Global").GetComponent<scrGlobal>().file = Resources.Load("Texts/dual_2A") as TextAsset;
+        GameObject.Find("Global").GetComponent<scrGlobal>().levelNum = 2;
+    }
+
+}

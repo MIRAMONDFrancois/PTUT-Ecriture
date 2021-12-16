185c810a32b2f6b6b0d3966f1eb8c9e4ebea3500
MIRAMONDFrancois <francois.miramond@etud.univ-jfc.fr>
2021-12-16 17:02:04 +0100

    pimp my menu ep2

---
+++ MenuAPoint/Assets/Scripts/scrButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrButton : MonoBehaviour
+{
+    private bool isTargeted;
+    private float normalScale = 1f;
+    private float upScale = 1.2f;
+    private float speedScale = 0.03f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTargeted = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isTargeted && (transform.localScale.x < upScale)) {
+            Vector3 newTrans = gameObject.transform.localScale;
+            newTrans.x += speedScale;
+            newTrans.y += speedScale;
+            gameObject.transform.localScale = newTrans;
+        } 
+        if (!isTargeted && (transform.localScale.x > normalScale)) {
+            Vector3 newTrans = gameObject.transform.localScale;
+            newTrans.x -= speedScale;
+            newTrans.y -= speedScale;
+            gameObject.transform.localScale = newTrans;
+        }
+    }
+
+    public void SetTargeted(bool value) {
+        isTargeted = value;   
+    }
+}

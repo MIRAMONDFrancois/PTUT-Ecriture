02c41df44f3e1766f2383e516ed9ae93d24aa803
benjik81 <benjamin.zanini@outlook.com>
2022-02-25 10:15:46 +0100

    Script indices

--- MenuAPoint/Assets/Scripts/scrIndice.cs
+++ MenuAPoint/Assets/Scripts/scrIndice.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class scrIndice : MonoBehaviour
 {
+    public GameObject Indice;
+
+    void Start()
+    {
+        level3unlocked();
+    }
+
     public void decrementIndice()
     {
         if (GameObject.Find("Global").GetComponent<scrGlobal>().nbIndices > 0)
             GameObject.Find("Global").GetComponent<scrGlobal>().nbIndices--;
+    }
+
+    public void level3unlocked()
+    {
+        if(GameObject.Find("Global").GetComponent<scrGlobal>().levelNum > 3)
+        {
+            
+            Indice.SetActive(true);
+        }
     }
 }

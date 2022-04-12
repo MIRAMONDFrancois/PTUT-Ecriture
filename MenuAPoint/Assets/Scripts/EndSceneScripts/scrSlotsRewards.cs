b4a9e2c627a196464de2abd6797e2825ee927a40
Benjik <ben12pr@hotmail.com>
2022-04-12 17:02:36 +0200

    {Add} scrSlotsRewards Script
    Added script to control slots & drop.

---
+++ MenuAPoint/Assets/Scripts/EndSceneScripts/scrSlotsRewards.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class scrSlotsRewards : MonoBehaviour, IDropHandler
+{
+    public static bool pointerIsOnSlot = false;
+    public static string nameSlot;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        nameSlot = name;
+        if (eventData.pointerDrag != null)
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+    }
+
+    public void PointerOnSlot()
+    {
+        pointerIsOnSlot = true;
+    }
+
+    public void PointerOutSlot()
+    {
+        pointerIsOnSlot = false;
+    }
+}

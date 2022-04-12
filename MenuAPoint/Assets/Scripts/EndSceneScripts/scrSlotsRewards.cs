ab1f968d4c8d31b566167cc139e30cd687ff6acc
Benjik <ben12pr@hotmail.com>
2022-04-12 20:43:51 +0200

    {Update} scrSlotsRewards
    If the item cannot move, the drag & drop is now impossible.

--- MenuAPoint/Assets/Scripts/EndSceneScripts/scrSlotsRewards.cs
+++ MenuAPoint/Assets/Scripts/EndSceneScripts/scrSlotsRewards.cs
@@ -11,8 +11,52 @@ public class scrSlotsRewards : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         nameSlot = name;
-        if (eventData.pointerDrag != null)
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+
+        switch (name)
+        {
+            case "SlotCasserole":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[0])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotCuillere":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[1])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotCouteau":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[2])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotFouet":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[3])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotLouche":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[4])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotPassoire":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[5])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotPoele":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[6])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotRouleau":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[7])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotRape":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[8])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            case "SlotSpatule":
+                if (eventData.pointerDrag != null && GameObject.Find("Global").GetComponent<scrGlobal>().endSceneItemsCanMove[9])
+                    eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                break;
+            default:
+                throw new System.Exception("Element not found");
+        }
     }
 
     public void PointerOnSlot()

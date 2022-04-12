eb1232ff84ec10c4f040aa3e53d3080e0b0e3c89
Benjik <ben12pr@hotmail.com>
2022-04-12 21:03:33 +0200

    {Update} scrEnd Script
    Display or not UI elements

--- MenuAPoint/Assets/Scripts/scrEnd.cs
+++ MenuAPoint/Assets/Scripts/scrEnd.cs
@@ -5,11 +5,25 @@ using TMPro;
 
 public class scrEnd : MonoBehaviour
 {
-    public TextMeshProUGUI TextFin;
+    public TextMeshProUGUI textFin;
+    public GameObject menuButton;
+    public TextMeshProUGUI titreUI;
+    public TextMeshProUGUI instructionsUI;
 
     private void Start()
     {
         checkIndices();
+    }
+
+    private void Update()
+    {
+       if(GameObject.Find("Global").GetComponent<scrGlobal>().nbrDrag > 9)
+        {
+            textFin.gameObject.SetActive(true);
+            menuButton.SetActive(true);
+            titreUI.gameObject.SetActive(true);
+            instructionsUI.gameObject.SetActive(false);
+        }
     }
 
     public void checkIndices()
@@ -17,22 +31,22 @@ public class scrEnd : MonoBehaviour
         switch (GameObject.Find("Global").GetComponent<scrGlobal>().nbIndices)
         {
             case 0:
-                TextFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière surprenante !";
+                textFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière surprenante !";
                 break;
             case 1:
-                TextFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière remarquable !";
+                textFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière remarquable !";
                 break;
             case 2:
-                TextFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière épatante !";
+                textFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière épatante !";
                 break;
             case 3:
-                TextFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière géniale !";
+                textFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière géniale !";
                 break;
             case 4:
-                TextFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière formidable !";
+                textFin.text = "Grâce à votre aide, Tokki est devenue une cuisinière formidable !";
                 break;
             case 5:
-                TextFin.text = "Grâce à votre aide, Tokki a réussi à réaliser son rêve ultime ; elle est devenue une excellente cuisinière";
+                textFin.text = "Grâce à votre aide, Tokki a réussi à réaliser son rêve ultime ; elle est devenue une excellente cuisinière";
                 break;
             default:
                 throw new System.Exception("Erreur");

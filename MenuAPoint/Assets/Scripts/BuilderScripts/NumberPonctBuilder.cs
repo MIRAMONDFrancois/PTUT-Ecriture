147103f81f60b38a6d3bf730cf172e9c198af095
Lipoulpe <60692322+Lipoulpe@users.noreply.github.com>
2022-04-13 16:48:11 +0200

    {Add} Builder ponct number

---
+++ MenuAPoint/Assets/Scripts/BuilderScripts/NumberPonctBuilder.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberPonctBuilder : MonoBehaviour
+{
+    public TMP_InputField PonctTexte;
+
+    public Transform[] ListePot;
+    private int[] nbPot = {0,0,0,0,0,0};
+
+    void Start()
+    {
+        StartCoroutine(ComptagePonct());
+    }
+
+    //(int pointLimit, int virguleLimit, int exclamationLimit,int interrogationLimit,int deuxpointsLimit,int pointvirguleLimit)
+    IEnumerator ComptagePonct()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1f);
+        
+            for(int a=0;a<nbPot.Length;a++)
+            {
+                nbPot[a]=0;
+            }
+            
+            for(int a=0;a<PonctTexte.text.Length;a++)
+            {
+                switch (PonctTexte.text[a])
+                {
+                    case '.':
+                        nbPot[0]++;
+                    break;
+                    case '!':
+                        nbPot[2]++;
+                    break;
+                    case '?':
+                        nbPot[3]++;
+                    break;
+                    case ',':
+                        nbPot[1]++;
+                    break;
+                    case ':':
+                        nbPot[4]++;
+                    break;
+                    case ';':
+                        nbPot[5]++;
+                    break;
+                }
+            }
+
+            ChangementNbPonct();
+
+
+        }
+        
+    }
+
+    private void ChangementNbPonct()
+    {
+        for(int a=0;a<ListePot.Length;a++)
+        {
+            PonctBuildEvents ponctTemp = ListePot[a].GetComponent<PonctBuildEvents>();
+
+            if(ponctTemp.InfiniteToggle)
+            {
+                ponctTemp.TextTot.text = "-1";
+            }else
+            {
+                ponctTemp.TextTot.text = ponctTemp.IntSuppl + nbPot[a] + "";
+            }
+        }
+    }
+
+
+
+}
---
+++ MenuAPoint/Assets/Scripts/BuilderScripts/Ponct/PonctBuildEvents.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class PonctBuildEvents : MonoBehaviour
+{
+    //nombre tot
+    public TextMeshProUGUI TextTot;
+
+    //nombre de ponct supplémentaire
+    public TextMeshProUGUI TextSuppl;
+    [HideInInspector] public int IntSuppl; 
+
+    //action sur image
+    public Image InfinitePonct;
+    public Image PlusPonct;
+    public Image MoinsPonct;
+    [HideInInspector] public bool InfiniteToggle;
+
+
+    public void TextMoins()
+    {
+        IntSuppl--;
+        TextSuppl.text = IntSuppl+"";
+
+        if(IntSuppl <= 0)
+        {
+            IntSuppl = 0;
+            MoinsPonct.gameObject.SetActive(false);
+        }
+    }
+
+    public void TextPlus()
+    {
+        IntSuppl++;
+        TextSuppl.text = IntSuppl+"";
+
+        MoinsPonct.gameObject.SetActive(true);
+    }
+
+    public void PonctInfinite()
+    {
+        if(InfiniteToggle)
+        {
+            InfinitePonct.color = Color.black;
+        }else
+        {
+            InfinitePonct.color = Color.green;
+        }
+
+        InfiniteToggle = !InfiniteToggle;
+        
+    }
+}

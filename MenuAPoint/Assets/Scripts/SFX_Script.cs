be5f7581cbbdfeb1ba2bf587f39bc83f95192175
Benjik <ben12pr@hotmail.com>
2022-04-15 17:00:00 +0200

    {Update} SFX_script
    Sounds

--- MenuAPoint/Assets/Scripts/SFX_Script.cs
+++ MenuAPoint/Assets/Scripts/SFX_Script.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class SFX_Script : MonoBehaviour
 {
     public AudioSource pop;
+    public AudioSource back;
+    public AudioSource indice;
+    public AudioSource clientconfu;
+    public AudioSource clientfade;
+    public AudioSource clientepice;
+    public AudioSource reussite;
 
     public void pop_block()
     {
@@ -12,5 +18,43 @@ public class SFX_Script : MonoBehaviour
         pop.clip = Resources.Load<AudioClip>("Sounds/pop"+popnb);
         
         pop.Play();
+    }
+    public void back_sound()
+    {
+        back.clip = Resources.Load<AudioClip>("Sounds/bouton_retour");
+
+        back.Play();
+    }
+
+    public void indice_sound()
+    {
+        indice.clip = Resources.Load<AudioClip>("Sounds/indices");
+
+        indice.Play();
+    }
+
+    public void client_confus()
+    {
+        clientconfu.clip = Resources.Load<AudioClip>("Sounds/client_confu");
+
+        clientconfu.Play();
+    }
+
+    public void client_fade()
+    {
+        clientfade.clip = Resources.Load<AudioClip>("Sounds/client_fade");
+        clientfade.Play();
+    }
+
+    public void client_epice()
+    {
+        clientepice.clip = Resources.Load<AudioClip>("Sounds/client_epice");
+        clientepice.Play();
+    }
+
+    public void reussite_sound()
+    {
+        reussite.clip = Resources.Load<AudioClip>("Sounds/reussite");
+        reussite.Play();
     }
 }

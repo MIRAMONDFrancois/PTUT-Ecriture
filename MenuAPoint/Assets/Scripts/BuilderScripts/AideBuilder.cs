739bda867b0b7f0ee58f1f0fdc8a3216499b8e00
Lipoulpe <60692322+Lipoulpe@users.noreply.github.com>
2022-04-20 17:19:16 +0200

    {Add} Aide Builder

---
+++ MenuAPoint/Assets/Scripts/BuilderScripts/AideBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AideBuilder : MonoBehaviour
+{
+    public GameObject Tuto;
+
+    void Start()
+    {
+        Tuto.transform.localScale = Vector3.zero;
+    }
+
+    public void Show()
+    {
+        Tuto.transform.localScale = new Vector2(1f,1f);
+    }
+
+    public void Hide()
+    {
+        Tuto.transform.localScale = Vector3.zero;
+    }
+}

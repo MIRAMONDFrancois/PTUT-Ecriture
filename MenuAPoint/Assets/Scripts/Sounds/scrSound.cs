a3d07e31830adefae7fcdc937127aa4c3c3f76eb
Benjik <ben12pr@hotmail.com>
2022-04-21 09:33:55 +0200

    {Add} scrSound / Sounds Folder
    Added scrSound to control if there is audio or not. Create a sounds folder and move SFX_Script.

---
+++ MenuAPoint/Assets/Scripts/Sounds/scrSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrSound : MonoBehaviour
+{
+    public GameObject toggle;
+    public void MuteAllSound()
+    {
+        if (!toggle.GetComponent<Toggle>().isOn)
+            AudioListener.volume = 0;
+        else
+            AudioListener.volume = 1;
+
+    }
+}

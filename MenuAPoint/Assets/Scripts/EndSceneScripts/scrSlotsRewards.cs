bfdf1b8a8d209737a71d4f1338d50d719f895538
Benjik <ben12pr@hotmail.com>
2022-10-24 22:33:23 +0200

    {Fix} scrSlotsRewards Script
    Moving the object slots is no longer possible.

--- MenuAPoint/Assets/Scripts/EndSceneScripts/scrSlotsRewards.cs
+++ MenuAPoint/Assets/Scripts/EndSceneScripts/scrSlotsRewards.cs
@@ -12,50 +12,50 @@ public class scrSlotsRewards : MonoBehaviour, IDropHandler
     {
         nameSlot = name;
 
-        switch (name)
+        switch (eventData.pointerDrag.name)
         {
-            case "SlotCasserole":
+            case "Casserole":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[0])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotCuillere":
+            case "Cuillere":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[1])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotCouteau":
+            case "Couteau":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[2])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotFouet":
+            case "Fouet":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[3])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotLouche":
+            case "Louche":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[4])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotPassoire":
+            case "Passoire":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[5])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotPoele":
+            case "Poele":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[6])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotRouleau":
+            case "Rouleau":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[7])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotRape":
+            case "Rape":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[8])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
-            case "SlotSpatule":
+            case "Spatule":
                 if (eventData.pointerDrag != null && scrGlobal.Instance.endSceneItemsCanMove[9])
                     eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
                 break;
             default:
-                throw new System.Exception("Element not found");
+                throw new System.Exception("Cet élément n'est pas un item.");
         }
     }
 
